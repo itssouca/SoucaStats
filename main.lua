239e1cf0888499e98c20cfa575538fd152cad31b
itssouca <itssouca@gmail.com>
2017-03-10 22:05:27 -0800

    Add heartbeats
    Added heartbeat functionality to log location and played time every 30
    and 900 seconds respectively. Played can likely be longer, but might be
    nescessary to keep played clock in sync with local clock. Can be removed
    if not needed. To accomplish this the event  handlers were pulled out
    and made named functions instead of anonymous ones created in the
    SetScript call. This allowed the location heartbeat to simply call the
    eventHandler with a fake HEARTBEAT_LOCATION event.

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-version = "00012"
+version = "00013"
 Listening_EventFrame = nil
 
 print( version )
@@ -11,6 +11,12 @@ logChatFrame = DEFAULT_CHAT_FRAME
 -- Make our frame for event registration
 local loaded = false
 local loggedIn = false
+
+local playedHeartbeatElapsed = 0
+local locationHeartbeatElapsed = 0
+
+local playedHeartbeatInterval = 300 -- 5 minutes
+local locationHeartbeatInterval = 30 -- 30 seconds
 
 
 -- build continent lookup map
@@ -53,217 +59,227 @@ Listening_EventFrame:RegisterEvent( "ADDON_LOADED" )
 
 
 
-Listening_EventFrame:SetScript( "OnEvent",
-	function( self, event, ... )
+
+function eventHandler( self, event, ... )
 --logChatFrame:AddMessage( "Event: |cFFFF0000" .. event .. "|r Time: |cFF00FF00" .. date( "%c", GetServerTime() ) .. "|r" )
 
-	local prettyLoaded = loaded and "Yes" or "No"
-	--logChatFrame:AddMessage( "loaded " .. prettyLoaded )
+local prettyLoaded = loaded and "Yes" or "No"
+--logChatFrame:AddMessage( "loaded " .. prettyLoaded )
 
-		if ( loaded == false ) then
-			if ( event == "ADDON_LOADED") then
-				local addonName = ...
+	if ( loaded == false ) then
+		if ( event == "ADDON_LOADED") then
+			local addonName = ...
 
-				if ( addonName == "SoucaStats" ) then
-					finishInit()
-					loaded = true
-				end
+			if ( addonName == "SoucaStats" ) then
+				finishInit()
+				loaded = true
 			end
-		else
-			local mapX, mapY = GetPlayerMapPosition( "player" )
-			local continentID = GetCurrentMapContinent()
-			local continent = continentID .. "-" .. continentMap[continentID]
-			local zoneName = GetRealZoneText() or "Zone"
-			local subZoneName = GetSubZoneText() or "Sub Zone"
-			local serverTime = GetServerTime()
-			local prettyTime = date( "%c", serverTime )
-			local onTaxi = UnitOnTaxi( "player" ) and "Y" or "N"
-			
-			local logHeader
-			local displayHeader
-
-			if ( loggedIn == true ) then
-				logHeader = event .. "|" .. serverTime .. "|" .. UnitLevel( "player" ) .. "|" ..
-						UnitXP( "player" ) .. "|" .. UnitXPMax( "player" ) .. "|" .. continentID ..
-						"|" .. zoneName .. "|" .. subZoneName .. "|" ..
-						mapX .. "," .. mapY .. "|" .. onTaxi
-				displayHeader = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
-						"< P Time: >" .. prettyTime .. "< Lvl: >" .. UnitLevel( "player" ) .. "< XP: >" .. UnitXP( "player" ) ..
-						"< Lvl XP: >" .. UnitXPMax( "player" ) .. "< Continent: >" .. continent .. "< Zone: >" ..
-						zoneName .. "::" .. subZoneName .. "< Loc: (" ..
-						mapX .. "," .. mapY .. ") Taxi: >" .. onTaxi .. "<"
-			else
-				logHeader = event .. "|" .. serverTime
-				displayHeader = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
-						"< P Time: >" .. prettyTime .. "<"
-			end
-
-						
-						
-			if (	event == "PLAYER_ALIVE" or
-					event == "PLAYER_DEAD" or
-					event == "PLAYER_UNGHOST" or
-					event == "PLAYER_CAMPING" ) then				
-
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )
-
-				RequestTimePlayed()
-
-
-			elseif ( event == "PLAYER_LEVEL_UP" ) then
-				local newLevel = ...
-				
-				local logEntry = logHeader .. "|" .. newLevel
-				local displayEntry = displayHeader .. " ### |cFF0099FFNew Lvl: >|cFFDD33FF" .. newLevel .. "|cFF0099FF<|r"
-
-				table.insert( SSEventLog.logs, logEntry )
-				logChatFrame:AddMessage( "SS: " .. displayEntry )
-
-				RequestTimePlayed()
-
-
-			elseif ( event == "TIME_PLAYED_MSG" ) then
-				local totalTime, levelTime = ...
-
-				local logEntry = logHeader .. "|" .. totalTime .. "|" .. levelTime
-				local displayEntry = displayHeader .. " ### |cFF0099FFPlayed: >|cFFDD33FF" .. totalTime ..
-									"|cFF0099FF< Lvl Played: >|cFFDD33FF" .. levelTime .. "|cFF0099FF<|r"
-
-				table.insert( SSEventLog.logs, logEntry )
-				logChatFrame:AddMessage( "SS: " .. displayEntry  )
-
-
-			elseif ( event == "QUEST_ACCEPTED" ) then
-				local questSlot, questId = ...		
-
-				local logEntry = logHeader .. "|" .. questSlot .. "|" .. questId
-				local displayEntry = displayHeader .. " ### |cFF0099FFQuest Slot: >|cFFDD33FF" .. questSlot ..
-						"|cFF0099FF< Quest ID: >|cFFDD33FF" .. questId .. "|cFF0099FF<|r"
-
-				table.insert( SSEventLog.logs, logEntry )
-				logChatFrame:AddMessage( "SS: " .. displayEntry  )
-				
-				RequestTimePlayed()
-			
-
-			elseif ( event == "QUEST_REMOVED" ) then
-				local questId = ...		
-
-				local logEntry = logHeader .. "|" .. questId			
-				local displayEntry = displayHeader .. " ### |cFF0099FFQuest ID: >|cFFDD33FF" .. questId .. "|cFF0099FF<|r"			
-
-				table.insert( SSEventLog.logs, logEntry )
-				logChatFrame:AddMessage( "SS: " .. displayEntry  )
-
-				RequestTimePlayed()
-
-
-			elseif ( event == "QUEST_TURNED_IN" ) then
-				local questId, questXP, questCopper = ...		
-
-				local logEntry = logHeader .. "|" .. questId .. "|" .. questXP .. "|" .. questCopper
-				local displayEntry = displayHeader .. " ### |cFF0099FFQuest ID: >|cFFDD33FF" .. questId ..
-						"|cFF000FF< QuestXP: >" .. questXP .. "|cFF0099FF< QuestCopper: >|cFFDD33FF" ..
-						questCopper .. "|cFF0099FF<|r"
-
-				table.insert( SSEventLog.logs, logEntry )
-				logChatFrame:AddMessage( "SS: " .. displayEntry  )
-
-				RequestTimePlayed()
-
-
-			elseif ( event == "PLAYER_FLAGS_CHANGED" ) then		
-				local unit = ...
-				
-				if ( unit == "player" ) then
-					local afkState = UnitIsAFK( "player" ) and "Y" or "N"
-
-					local logEntry = logHeader .. "|" .. afkState
-					local displayEntry = displayHeader .. " ### |cFF0099FFAFK: >|cFFDD33FF" .. afkState ..
-										"|cFF0099FF<|r"	
-
-					table.insert( SSEventLog.logs, logEntry )
-					logChatFrame:AddMessage( "SS" .. displayEntry  )
-				end
-				-- RequestTimePlayed()
-
-
-			elseif ( event == "UPDATE_CHAT_WINDOWS" ) then		
-				updateLoggingChatWindow()	
-
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )
-
-
-			elseif ( event == "PLAYER_LOGIN" ) then						
-				loggedIn = true
-
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )
-
-
-			elseif ( event == "PLAYER_LOGOUT" ) then						
-				loggedIn = false
-				
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )
-
-
-			-- Generic log entries go here
-			elseif ( 	event == "UNIT_QUEST_LOG_CHANGED" or
-						event == "ZONE_CHANGED_NEW_AREA" or
-						event == "ZONE_CHANGED" ) then		
-
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )	
-
-			else
-				
-				table.insert( SSEventLog.logs, logHeader )
-				logChatFrame:AddMessage( "SS: " .. displayHeader  )	
-			end
-
-		end 
-		
-	end
-	)
-
-
-Debugging_EventFrame:SetScript( "OnEvent",
-	function( self, event, ... )
-		--logChatFrame:AddMessage( "Event: |cFFFF0000" .. event .. "|r Time: |cFF00FF00" .. date( "%c", GetServerTime() ) .. "|r" )
-
+		end
+	else
+		local mapX, mapY = GetPlayerMapPosition( "player" )
+		local continentID = GetCurrentMapContinent()
+		local continent = continentID .. "-" .. continentMap[continentID]
+		local zoneName = GetRealZoneText() or "Zone"
+		local subZoneName = GetSubZoneText() or "Sub Zone"
 		local serverTime = GetServerTime()
 		local prettyTime = date( "%c", serverTime )
+		local onTaxi = UnitOnTaxi( "player" ) and "Y" or "N"
 		
-		local logEntry = event .. "|" .. serverTime
-		local displayEntry = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
-					"< P Time: >" .. prettyTime .. "< ###"
-		
-		local k, v
-		for k, v in pairs( {...} ) do
-			if ( type( v ) == "boolean" ) then
-				prettyV = v and "true" or "false"
-			else
-				prettyV = v
-			end
+		local logHeader
+		local displayHeader
 
-			logEntry = logEntry .. "|" .. k .. "*" .. prettyV
-			displayEntry = displayEntry .. " |cFF0099FFarg[|cFFDD33FF" .. k .. "|cFF0099FF]: >|cFFDD33FF" .. prettyV .. "|cFF0099FF<|r"
+		if ( loggedIn == true ) then
+			logHeader = event .. "|" .. serverTime .. "|" .. UnitLevel( "player" ) .. "|" ..
+					UnitXP( "player" ) .. "|" .. UnitXPMax( "player" ) .. "|" .. continentID ..
+					"|" .. zoneName .. "|" .. subZoneName .. "|" ..
+					mapX .. "," .. mapY .. "|" .. onTaxi
+			displayHeader = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
+					"< P Time: >" .. prettyTime .. "< Lvl: >" .. UnitLevel( "player" ) .. "< XP: >" .. UnitXP( "player" ) ..
+					"< Lvl XP: >" .. UnitXPMax( "player" ) .. "< Continent: >" .. continent .. "< Zone: >" ..
+					zoneName .. "::" .. subZoneName .. "< Loc: (" ..
+					mapX .. "," .. mapY .. ") Taxi: >" .. onTaxi .. "<"
+		else
+			logHeader = event .. "|" .. serverTime
+			displayHeader = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
+					"< P Time: >" .. prettyTime .. "<"
 		end
 
-		table.insert( SSEventLog.debugLogs, logEntry )
-		logChatFrame:AddMessage( "Debug: " .. displayEntry  )
+					
+					
+		if (	event == "PLAYER_ALIVE" or
+				event == "PLAYER_DEAD" or
+				event == "PLAYER_UNGHOST" or
+				event == "PLAYER_CAMPING" ) then				
+
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )
+
+			RequestTimePlayed()
+
+
+		elseif ( event == "PLAYER_LEVEL_UP" ) then
+			local newLevel = ...
+			
+			local logEntry = logHeader .. "|" .. newLevel
+			local displayEntry = displayHeader .. " ### |cFF0099FFNew Lvl: >|cFFDD33FF" .. newLevel .. "|cFF0099FF<|r"
+
+			table.insert( SSEventLog.logs, logEntry )
+			logChatFrame:AddMessage( "SS: " .. displayEntry )
+
+			RequestTimePlayed()
+
+
+		elseif ( event == "TIME_PLAYED_MSG" ) then
+			local totalTime, levelTime = ...
+
+			local logEntry = logHeader .. "|" .. totalTime .. "|" .. levelTime
+			local displayEntry = displayHeader .. " ### |cFF0099FFPlayed: >|cFFDD33FF" .. totalTime ..
+								"|cFF0099FF< Lvl Played: >|cFFDD33FF" .. levelTime .. "|cFF0099FF<|r"
+
+			table.insert( SSEventLog.logs, logEntry )
+			logChatFrame:AddMessage( "SS: " .. displayEntry  )
+
+
+		elseif ( event == "QUEST_ACCEPTED" ) then
+			local questSlot, questId = ...		
+
+			local logEntry = logHeader .. "|" .. questSlot .. "|" .. questId
+			local displayEntry = displayHeader .. " ### |cFF0099FFQuest Slot: >|cFFDD33FF" .. questSlot ..
+					"|cFF0099FF< Quest ID: >|cFFDD33FF" .. questId .. "|cFF0099FF<|r"
+
+			table.insert( SSEventLog.logs, logEntry )
+			logChatFrame:AddMessage( "SS: " .. displayEntry  )
+			
+			RequestTimePlayed()
+		
+
+		elseif ( event == "QUEST_REMOVED" ) then
+			local questId = ...		
+
+			local logEntry = logHeader .. "|" .. questId			
+			local displayEntry = displayHeader .. " ### |cFF0099FFQuest ID: >|cFFDD33FF" .. questId .. "|cFF0099FF<|r"			
+
+			table.insert( SSEventLog.logs, logEntry )
+			logChatFrame:AddMessage( "SS: " .. displayEntry  )
+
+			RequestTimePlayed()
+
+
+		elseif ( event == "QUEST_TURNED_IN" ) then
+			local questId, questXP, questCopper = ...		
+
+			local logEntry = logHeader .. "|" .. questId .. "|" .. questXP .. "|" .. questCopper
+			local displayEntry = displayHeader .. " ### |cFF0099FFQuest ID: >|cFFDD33FF" .. questId ..
+					"|cFF000FF< QuestXP: >" .. questXP .. "|cFF0099FF< QuestCopper: >|cFFDD33FF" ..
+					questCopper .. "|cFF0099FF<|r"
+
+			table.insert( SSEventLog.logs, logEntry )
+			logChatFrame:AddMessage( "SS: " .. displayEntry  )
+
+			RequestTimePlayed()
+
+
+		elseif ( event == "PLAYER_FLAGS_CHANGED" ) then		
+			local unit = ...
+			
+			if ( unit == "player" ) then
+				local afkState = UnitIsAFK( "player" ) and "Y" or "N"
+
+				local logEntry = logHeader .. "|" .. afkState
+				local displayEntry = displayHeader .. " ### |cFF0099FFAFK: >|cFFDD33FF" .. afkState ..
+									"|cFF0099FF<|r"	
+
+				table.insert( SSEventLog.logs, logEntry )
+				logChatFrame:AddMessage( "SS" .. displayEntry  )
+			end
+			-- RequestTimePlayed()
+
+
+		elseif ( event == "UPDATE_CHAT_WINDOWS" ) then		
+			updateLoggingChatWindow()	
+
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )
+
+
+		elseif ( event == "PLAYER_LOGIN" ) then						
+			loggedIn = true
+
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )
+
+
+		elseif ( event == "PLAYER_LOGOUT" ) then						
+			loggedIn = false
+			
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )
+
+
+		-- Generic log entries go here
+		elseif ( 	event == "UNIT_QUEST_LOG_CHANGED" or
+					event == "ZONE_CHANGED_NEW_AREA" or
+					event == "ZONE_CHANGED" or
+					event == "HEARTBEAT_LOCATION" ) then		
+
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )	
+
+		else
+			
+			table.insert( SSEventLog.logs, logHeader )
+			logChatFrame:AddMessage( "SS: " .. displayHeader  )	
+		end
+
+	end 
+	
+end
+
+-- Set the event script for our main event frame
+Listening_EventFrame:SetScript( "OnEvent", eventHandler )
+
+
+
+
+function debugHandler( self, event, ... )
+	--logChatFrame:AddMessage( "Event: |cFFFF0000" .. event .. "|r Time: |cFF00FF00" .. date( "%c", GetServerTime() ) .. "|r" )
+
+	local serverTime = GetServerTime()
+	local prettyTime = date( "%c", serverTime )
+	
+	local logEntry = event .. "|" .. serverTime
+	local displayEntry = "|cFFFF0000" .. event .. "|r - Time: >" .. serverTime ..
+				"< P Time: >" .. prettyTime .. "< ###"
+	
+	local k, v
+	for k, v in pairs( {...} ) do
+		if ( type( v ) == "boolean" ) then
+			prettyV = v and "true" or "false"
+		else
+			prettyV = v
+		end
+
+		logEntry = logEntry .. "|" .. k .. "*" .. prettyV
+		displayEntry = displayEntry .. " |cFF0099FFarg[|cFFDD33FF" .. k .. "|cFF0099FF]: >|cFFDD33FF" .. prettyV .. "|cFF0099FF<|r"
 	end
-	)
+
+	table.insert( SSEventLog.debugLogs, logEntry )
+	logChatFrame:AddMessage( "Debug: " .. displayEntry  )
+end
+
+Debugging_EventFrame:SetScript( "OnEvent", debugHandler )
 
 
 function updateLoggingChatWindow()
 	for i = 1, NUM_CHAT_WINDOWS do
-	 	if GetChatWindowInfo( i ) == "SS Log" then
+		local windowName = GetChatWindowInfo( i )
+
+		logChatFrame:AddMessage( "ChatWindow[|cFFDD33FF" .. i .. "|r]: \"|cFFDD33FF" .. windowName .. "|r\"" )
+
+	 	if  windowName == "SS Log" then
+	 		logChatFrame:AddMessage( "Switching output to \"SS Log\" frame at index " .. i )
 			logChatFrame = _G["ChatFrame" .. i]
-			logChatFrame:AddMessage( "Using SS Log frame" )
+			logChatFrame:AddMessage( "Chat frame switched" )
 			break
 		end
 	end
@@ -361,7 +377,7 @@ function finishInit()
 	Listening_EventFrame:RegisterEvent( "ZONE_CHANGED_NEW_AREA" )
 
 	-- Debugging event logs
-	Debugging_EventFrame:RegisterEvent( "TIME_PLAYED_MSG" )
+	-- Debugging_EventFrame:RegisterEvent( "TIME_PLAYED_MSG" )
 
 	Debugging_EventFrame:RegisterEvent( "GROUP_ROSTER_UPDATE" )
 	Debugging_EventFrame:RegisterEvent( "PARTY_INVITE_REQUEST" )
@@ -381,6 +397,35 @@ function finishInit()
 
 end
 
+
+-- OnUpdate function to perform heartbeat actions
+function heartbeatHandler( self, elapsed )
+	playedHeartbeatElapsed = playedHeartbeatElapsed + elapsed
+	locationHeartbeatElapsed = locationHeartbeatElapsed + elapsed
+
+	if ( playedHeartbeatElapsed > playedHeartbeatInterval ) then
+		RequestTimePlayed()
+
+		-- reset played and location because played event will log location as well
+		playedHeartbeatElapsed = 0
+		locationHeartbeatElapsed = 0
+	elseif ( locationHeartbeatElapsed > locationHeartbeatInterval ) then
+		eventHandler( self, "HEARTBEAT_LOCATION")
+
+		-- Only reset location so that we will still trigger played later
+		locationHeartbeatElapsed = 0
+	end
+
+
+
+end
+
+
+
+Listening_EventFrame:SetScript( "OnUpdate", heartbeatHandler )
+
+
+
 -- UI_INFO_MESSAGE
 -- Arg1: 287 ??
 -- Arg2: "Lynx Collar: 8/8"
@@ -388,3 +433,7 @@ end
 -- UI_INFO_MESSAGE
 -- Arg1: 284 ??
 -- Arg2: "Objective Complete"
+
+
+
+-- /run for i = 1, NUM_CHAT_WINDOWS do print( "[" .. i .. "][" .. GetChatWindowInfo( i ) .. "]" ) end
